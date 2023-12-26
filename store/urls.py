94b3796155bab1ebfb34316c0b6883194719b88a
egordovsky <stepnoy-volk@yandex.ru>
2023-12-26 22:20:30 +0300

    Add "other" and "store" apps

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import ProductSingleView, CartView, ShopView
+
+app_name = 'store'
+
+urlpatterns = [
+    path('', ShopView.as_view(), name='shop'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('product/<int:id>', ProductSingleView.as_view(), name='product'),
+
+
+]
